873c86f29dea3c22e863cf330edd054bacf9a9b9
Naing Linn Phyo <nainglinnphyo24@gmail.com>
2024-07-28 00:42:19 +0630

    chore: optimize animation

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,10 +10,20 @@ export function Providers({ children }: { children: React.ReactNode }) {
         <div className="absolute right-0 top-72 -z-10">
           <LineAnimationTwo />
         </div>
-        <div className="absolute left-0 top-72 -z-10">
+        <div className="absolute left-0 top-72 -z-10 lg:block hidden">
           <LineAnimationTwo />
         </div>
-        <div className="absolute right-60 top-72 -z-10">
+        <div className="absolute right-64 top-72 -z-10 lg:block hidden">
+          <LineAnimationTwo />
+        </div>
+
+        <div className="absolute right-0 bottom-40 2 -z-10">
+          <LineAnimationTwo />
+        </div>
+        <div className="absolute left-0 bottom-40 -z-10 lg:block hidden">
+          <LineAnimationTwo />
+        </div>
+        <div className="absolute right-64 bottom-40 -z-10 lg:block hidden">
           <LineAnimationTwo />
         </div>
         {children}
