7f65705e90d3151e9f97013c0ee4722f91296491
nainglinnphyo <nainglinnphyo24@gmail.com>
2024-09-10 20:08:09 +0630

    feat: add footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Home from '@/components/Home';
 import Exp from '@/components/Exp';
 import Link from 'next/link';
 import React from 'react';
+import Footer from '@/components/Footer';
 
 function Main() {
      return (
           <div className='flex flex-col space-y-10'>
                <Home />
                <Exp />
+               <Footer />
           </div>
      );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+     return (
+          <div className='py-4'>
+               <h2>Naing Linn Phyo</h2>
+               <h3>Team Lead | Backend Developer | Web Developer</h3>
+               <div className='flex'>
+                    <h2>Connect with me: </h2>
+                    <a href="https://www.linkedin.com/in/dev-nainglinnphyo" target='_blank'> LinkedIn</a> |
+                    <a href="https://github.com/nainglinnphyo" target='_blank'>Github</a> |
+                    <a href="mailto:nainglinnphyo24@gmail.com">Email</a>
+               </div>
+               <p className='text-sm font-bold'>© 2024 Naing Linn Phyo. All rights reserved.</p>
+          </div>
+     )
+}
+
+export default Footer
